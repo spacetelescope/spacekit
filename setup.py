929c63176e28036f22f3a889b15a7f0f6db98b47
Hakkeray <rukeine@gmail.com>
2020-10-11 15:52:00 -0700

    updated dependencies

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'requests==2.18.4',
         'pandas==0.25.1',
         'numpy=1.16.5',
-        'scikit-learn=0.21.2'
+        'scikit-learn=0.21.2',
         'scipy=1.3.1',
         'tensorflow==2.2.0'
     ],
