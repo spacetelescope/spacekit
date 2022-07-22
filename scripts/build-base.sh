8c20e588c7b3da64e75cebf9356d1315ca21dffb
alphasentaurii <rkein@stsci.edu>
2022-07-22 00:19:12 -0400

    remove uncompressed test data

--- scripts/build-base.sh
+++ scripts/build-base.sh
@@ -3,18 +3,10 @@
 # Typically you only would need to customize the dashboard layer image built on top of this.
 # If you do customize and build the base image, be sure to specify it in your .env config
 # The base image is pulled from docker hub automatically if/when you build the dashboard image
-ENV=$1
-
-if [[ $ENV == "dev" ]]; then
-    reqs="./docker/images/base_image/requirements-dev.txt"
-    echo "ENV=${ENV}: building deps from ${reqs}"
-else
-    reqs="./docker/images/base_image/requirements.txt"
-    echo "ENV=prod: building deps from ${reqs}"
-fi
+spacekit_version=$1
 
 export DOCKER_IMAGE=alphasentaurii/spacekit:base
 export BASE_IMG="debian:bullseye-slim"
 export BASE_DOCKERFILE="./docker/images/base_image/Dockerfile"
 
-docker build -f ${BASE_DOCKERFILE} -t ${DOCKER_IMAGE} --build-arg BASE_IMAGE="${BASE_IMG}" --build-arg REQ_FILE=$reqs .
+docker build -f ${BASE_DOCKERFILE} -t ${DOCKER_IMAGE} --build-arg BASE_IMAGE=${BASE_IMG} --build-arg SPACEKIT_VERSION=${spacekit_version} .
--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,14 @@ if [[ "${CFG}" != "dev" ]]; then
     --build-arg BASE_IMAGE="${BASE_IMG}" \
     --build-arg SRC="${SRC}" \
     --build-arg COLLECTION="${COLLECTION}" \
+    --build-arg PFX="${PFX}" \
     --build-arg DATASETS=${DATASETS} .
 else
     DOCKER_FILE=./docker/images/dashboard_image/templates/dev/Dockerfile
     docker build -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} \
     --build-arg BASE_IMAGE="${BASE_IMG}" \
-    --build-arg SPACEKIT_REPO="${SPACEKIT_REPO}" \
-    --build-arg SPACEKIT_BRANCH="${SPACEKIT_BRANCH}" \
+    --build-arg SPACEKIT_REF="${SPACEKIT_BRANCH}" \
+    --build-arg PFX="${PFX}" \
     --build-arg SRC="${SRC}" \
     --build-arg COLLECTION="${COLLECTION}" \
     --build-arg DATASETS=${DATASETS} .
