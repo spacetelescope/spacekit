5b2d4e069e41674eb89b65151249a66076960a31
alphasentaurii <rkein@stsci.edu>
2022-07-24 14:26:01 -0400

    remove baseimg build arg

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,6 @@ source "./docker/images/dashboard_image/.env"
 DOCKER_FILE=./docker/images/dashboard_image/Dockerfile
 
 docker build -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} \
---build-arg BASE_IMAGE="${BASE_IMG}" \
 --build-arg SPACEKIT_VERSION="${SPACEKIT_VERSION}"
 --build-arg SRC="${SRC}" \
 --build-arg COLLECTION="${COLLECTION}" \
