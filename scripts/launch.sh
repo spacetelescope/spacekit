6e6789478bd9965cee36ecd20615f86b9019a5da
alphasentaurii <rkein@stsci.edu>
2023-03-22 14:47:23 -0400

    wildcard for additional launch vars

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -8,11 +8,11 @@ fi
 
 if [ $MOUNTS -ne 0 ]; then
     docker run ${CONTAINER_MODE} --name $NAME \
-    --ip $IPADDRESS --hostname $HOSTNAME -p 8050:8050 \
+    --ip $IPADDRESS --hostname $HOSTNAME -p 8050:8050 $* \
     --mount type=bind,source=${SOURCEDATA},target=${DESTDATA} \
     $DOCKER_IMAGE $EPCOMMAND
 else
     docker run ${CONTAINER_MODE} --name $NAME \
-    --ip $IPADDRESS --hostname $HOSTNAME -p 8050:8050 \
+    --ip $IPADDRESS --hostname $HOSTNAME -p 8050:8050 $* \
     $DOCKER_IMAGE $EPCOMMAND
 fi
