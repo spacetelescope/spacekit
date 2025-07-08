b971c654d06a8ee7e23ab9fd39e330dc0fcb701a
alphasentaurii <rkein@stsci.edu>
2025-07-08 14:53:04 -0400

    move extra info to docs page

--- docs/source/preprocessor/ingest.rst
+++ docs/source/preprocessor/ingest.rst
@@ -6,16 +6,33 @@ spacekit.preprocessor.ingest
 
 .. currentmodule:: spacekit.preprocessor.ingest
 
+Jwst Calibration Data Ingest
+----------------------------
+
+At STSCI, additional model training data is acquired daily from the telescope's calibration pipeline. Due to the nature of an automated 24-hour data collection cycle, some Level 3 products may still be processing at the time data is collected. This results in a given input file containing groups of L1 exposures with no matching L3 product. JwstCalIngest will run preprocessing on all L1 inputs and attempt to match them with an L3 product in the same file. 
+
+- Any complete datasets (where a match is identified) are inserted into the "database", a file called `training.csv`. 
+- Any remaining L1 exposures that did not find a match are stored into a separate "table" called `ingest.csv`. 
+
+The next time this ingest process is run, the script will load both the new data as well as prior (unmatched) data. The assumption here is that the missing L3 product(s) (and sometimes even additional L1 exposures for this association) will eventually complete the pipeline and show up in subsequent files.
+
+Additional output files are model-specific encoded subsets of `preprocessed` and `ingest`. Data is inserted into these in the same manner as appropriate. The actual files to be used for model training are named as `train-{modelname}.csv`, while `training.csv` contains all the original columns with unencoded values and is intended to be used primarily for data analysis and debugging purposes.
+
+    - Database: {outpath}
+    - Tables: {.csv files}
+         - Accumulated data storing unencoded values
+            - preprocessed:  complete L1-L3 groupings
+            - ingest: unmatched L1 exposures
+            - mosaics: c1XXX association candidate L3 products (currently not supported)
+        - Encoded datasets finalized and ready for model training (input features + y-targets)
+            - train-image: L3 image model
+            - train-spec: L3 spectroscopy model
+            - train-tac: L3 TSO/AMI/CORON model
+        - Encoded input features of remaining L1 exposures (y-targets pending)
+            - rem-image.csv
+            - rem-spec.csv
+            - rem-tac.csv
+
+
 .. automodule:: spacekit.preprocessor.ingest
    :members:
-
-
-.. autoclass:: JwstCalIngest
-   :members:
-   :undoc-members:
-   :show-inheritance:
-
-.. autoclass:: SvmAlignmentIngest
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- spacekit/preprocessor/ingest.py
+++ spacekit/preprocessor/ingest.py
@@ -196,47 +196,21 @@ class JwstCalIngest:
     This outpath acts as the primary database containing several "tables" (dataframes stored
     in .csv files). This class is designed to run on single or multiple files at a time 
     (limit specificity using 'pfx`). 
+    
     Input file naming convention: YYYY-MM-DD_%d.csv (%d = day of year) ex: 2024-02-21_052.csv
     Alternate formats currently not supported because filenames are used to store date info.
-    Examples: To ingest multiple files from November 2023, set `pfx="2023-11". To ingest only
-    one file from January 3, 2024, set `pfx="2024-01-03". You can also pass in a wildcard, for
-    example `pfx="*_3" would search for all data collected on days 300-365 of any year,
-    while `pfx="2023*_3" would do the same but only for the year 2023.
+    Examples: 
+    
+    - To ingest multiple files from November 2023, set `pfx="2023-11"`. 
+    - To ingest only one file from January 3, 2024, set `pfx="2024-01-03"`. 
+    - You can also pass in a wildcard: `pfx="*_3" would search for all data collected on days 300-365 of any year,
+    while `pfx="2023*_3"` would do the same but only for the year 2023.
+
     The contents of raw metadata files are expected to contain:
 
-        1) columns consistent with Fits header keyword-values used in JWST Cal model training 
-        (see `spacekit.skopes.jwst.cal.config`) 
-        2) rows of Level 1/1b exposures (inputs/features) along with Level 3 products
-        3) imagesize (memory footprint) for each L3 product (outputs/target)
-    
-    At STSCI, additional model training data is acquired daily from the telescope's calibration pipeline.
-    Due to the nature of an automated 24-hour data collection cycle, some Level 3 products may still be
-    processing at the time data is collected. This results in a given input file containing groups of L1
-    exposures with no matching L3 product. JwstCalIngest will run preprocessing on all L1 inputs and attempt 
-    to match them with an L3 product in the same file. Any complete datasets (where a match is identified) are
-    inserted into the "database", a file called `training.csv`. Any remaining L1 exposures that did not 
-    find a match are stored into a separate "table" called `ingest.csv`. The next time this ingest process
-    is run, the script will load both the new data as well as prior (unmatched) data. The assumption here is
-    that the missing L3 product(s) (and sometimes even additional L1 exposures for this association) will 
-    eventually complete the pipeline and show up in subsequent files.
-    Additional output files are model-specific encoded subsets of `preprocessed` and `ingest`. Data is inserted
-    into these in the same manner as appropriate. The actual files to be used for model training are named as
-    "train-{modelname}.csv", while `training.csv` contains all the original columns with unencoded values
-    and is intended to be used primarily for data analysis and debugging purposes.
-    Database: {outpath}
-    Tables: {.csv files}
-        Accumulated data storing unencoded values
-        - preprocessed:  complete L1-L3 groupings
-        - ingest: unmatched L1 exposures
-        - mosaics: c1XXX association candidate L3 products (currently not supported)
-        Encoded datasets finalized and ready for model training (input features + y-targets)
-        - train-image: L3 image model
-        - train-spec: L3 spectroscopy model
-        - train-tac: L3 TSO/AMI/CORON model
-        Encoded input features of remaining L1 exposures (y-targets pending)
-        - rem-image.csv
-        - rem-spec.csv
-        - rem-tac.csv
+        - 1) columns consistent with Fits header keyword-values used in JWST Cal model training (see `spacekit.skopes.jwst.cal.config`) 
+        - 2) rows of Level 1/1b exposures (inputs/features) along with Level 3 products
+        - 3) imagesize (memory footprint) for each L3 product (outputs/target)
 
     Parameters
     ----------
